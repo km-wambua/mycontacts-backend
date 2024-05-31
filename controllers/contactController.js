759d83a6c17309fb5f27224eeb3394529653c0e1
Kisaa Manasseh Wambua <mmusyocks@gmail.com>
2024-05-31 12:21:53 +0300

    Implemented public and private routing. Linked project to a MongoDB database. Introduced authentication and authorization in private routes

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,36 +1,73 @@
+const asyncHandler = require("express-async-handler")
+const Contact = require("../models/contactModel")
 // @desc Get all contacts
 // @route GET /api/contacts
 // @access public
 
-const getContacts = (req, res) => {
-    res.status(200).json({message: "Get all contacts"});
-}
+const getContacts = asyncHandler (async (req, res) => {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+});
 
 // @desc Get a single contact
 // @route GET /api/contact/:id
 // @access public
-const getContact = (req, res) => {
-    res.status(200).json({message: `Get single contact of the id ${req.params.id}`})
-}
+const getContact = asyncHandler (async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+
+    if(!contact) {
+        res.status(404)
+        throw new Error("Contact not found");
+    }
+    res.status(200).json(contact);
+});
 
 // @desc create a contact
 // @route POST /api/contact
 // @access public
-const createContact = (req, res) => {
-    res.status(200).json({message: "Create a new contact"})
-}
+const createContact = asyncHandler (async (req, res) => {
+    console.log("The request body is:", req.body)
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    };
+    const contact = await Contact.create({
+        name,
+        email,
+        phone
+    });
+    res.status(201).json({contact});
+});
 
 // @desc update a  contact
 // @route PUT /api/contact/:id
 // @access public
-const updateContact = (req, res) => {
-    res.status(200).json({message: `Update contact of the id ${req.params.id}`})
-}
+const updateContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id)
+    if(!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    };
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    );
+    res.status(200).json(updatedContact);
+});
 
 // @desc delete  contact
 // @route DELETE /api/contact/:id
 // @access public
-const deleteContact = (req, res) => {
-    res.status(200).json({message: `Delete contact of the id ${req.params.id}`})
-}
+const deleteContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if(!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    await Contact.deleteOne({_id: req.params.id});
+    res.status(200).json(contact)
+});
+
 module.exports = { getContacts, getContact, createContact, updateContact, deleteContact }
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+const contactRoutes = require("./routes/contactRoutes");
+const userRoutes = require("./routes/userRoutes")
+const errorHandler = require("./middleware/errorHandler");
+
+connectDb();
 const app = express();
-const contactRoutes = require("./routes/contactRoutes")
 
 const port = process.env.PORT || 5000;
-
+app.use(express.json());
 app.use("/api/contacts", contactRoutes);
+app.use("/api/users", userRoutes);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
